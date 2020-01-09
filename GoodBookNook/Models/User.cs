7ee293f7dbfe672037ee41f83ee2a0b473cbf11b
Brian Bird <birdb@lanecc.edu>
2020-01-08 16:34:34 -0800

    Removed unused properties

--- GoodBookNook/Models/User.cs
+++ GoodBookNook/Models/User.cs
@@ -4,17 +4,8 @@ namespace GoodBookNook.Models
 {
     public class User
     {
-        private List<Review> reviews = new List<Review>();
-        private List<Comment> comments = new List<Comment>();
-
         public int UserID { get; set; }
         public string Name { get; set; }
         public string Email { get; set; }
-
-        // These are "navigational properties" that will cause
-        // EF to generate FK fields for UserID in the Review and Comment tables.
-        public List<Review> Reviews {get { return reviews; } }
-        public List<Comment> Comments { get { return comments; } }
-
     }
 }
