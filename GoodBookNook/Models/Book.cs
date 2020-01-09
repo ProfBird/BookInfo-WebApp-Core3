d17722d067023c459762f862f0d85e986fe078fb
profbird <bidb@lanecc.edu>
2020-01-09 11:56:26 -0800

    Added validation code

--- GoodBookNook/Models/Book.cs
+++ GoodBookNook/Models/Book.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -11,6 +12,7 @@ namespace GoodBookNook.Models
         private List<Author> authors = new List<Author>();
         private List<Review> reviews = new List<Review>();
 
+        [StringLength(100, MinimumLength = 2)]
         public string Title { get; set; }
         public DateTime PubDate { get; set; }
 
