ea7be577b6be2bd74224711b5c812f2f87a4003b
profbird <bidb@lanecc.edu>
2020-01-16 11:55:02 -0800

    added more views

--- GoodBookNook/Repositories/AppDbContext.cs
+++ GoodBookNook/Repositories/AppDbContext.cs
@@ -10,7 +10,6 @@ namespace GoodBookNook.Repositories
            DbContextOptions<AppDbContext> options) : base(options) { }
         public DbSet<Book> Books { get; set; }
         public DbSet<Author> Authors { get; set; }
-        // public DbSet<AppUser> Users { get; set; }
         public DbSet<Comment> Comments { get; set; }
         public DbSet<Review> Reviews { get; set; }
     }
