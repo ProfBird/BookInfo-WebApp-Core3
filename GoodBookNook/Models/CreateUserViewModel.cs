764b40e29991dc8c456441fa3c24b9b3f76664ab
profbird <bidb@lanecc.edu>
2020-01-16 10:57:04 -0800

    added a controller and view

---
+++ GoodBookNook/Models/CreateUserViewModel.cs
@@ -0,0 +1,15 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+
+namespace GoodBookNook.Models
+{
+    public class CreateUserViewModel
+    {
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Email { get; set; }
+        [Required]
+        public string Password { get; set; }
+    }
+}
