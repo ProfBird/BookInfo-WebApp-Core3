4125018c854f7cc7e22aa609a4c4ecbce819919f
profbird <bidb@lanecc.edu>
2020-01-23 09:51:59 -0800

    Added Login and Logout

---
+++ GoodBookNook/Controllers/AccountController.cs
@@ -0,0 +1,52 @@
+﻿using GoodBookNook.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace GoodBookNook.Controllers
+{
+    public class AccountController : Controller
+    {
+
+        private UserManager<AppUser> userManager;
+        private SignInManager<AppUser> signInManager;
+
+        [AllowAnonymous]
+        [HttpGet]
+        public IActionResult Login(string returnUrl)
+        {
+            ViewBag.returnUrl = returnUrl;
+            return View();
+        }
+
+        public AccountController(UserManager<AppUser> userMgr,
+                SignInManager<AppUser> signinMgr)
+        {
+            userManager = userMgr;
+            signInManager = signinMgr;
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Login(LoginViewModel model, string returnUrl)
+        {
+            if (ModelState.IsValid)
+            {
+                AppUser user = await userManager.FindByEmailAsync(model.Email);
+                if (user != null)
+                {
+                    await signInManager.SignOutAsync();
+                    var result = await signInManager.PasswordSignInAsync(user, model.Password, false, false);
+                    if (result.Succeeded)
+                    {
+                        return Redirect(returnUrl ?? "/");
+                    }
+                }
+                ModelState.AddModelError(nameof(LoginViewModel.Email), "Invalid user or password");
+            }
+            return View(model);
+        }
+    }
+}
---
+++ GoodBookNook/Models/LoginViewModel.cs
@@ -0,0 +1,17 @@
+﻿
+
+using System.ComponentModel.DataAnnotations;
+
+namespace GoodBookNook.Models
+{
+    public class LoginViewModel
+    {
+        [Required]
+        [UIHint("email")]
+        public string Email { get; set; }
+        [Required]
+        [UIHint("password")]
+        public string Password { get; set; }
+    }
+
+}
