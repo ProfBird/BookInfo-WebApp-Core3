af96f6df7f7cc4ff94f791a254a77cf5ca59f004
Brian Bird <birdb@lanecc.edu>
2020-02-13 16:00:17 -0800

    Published to Azure

--- GoodBookNook/Startup.cs
+++ GoodBookNook/Startup.cs
@@ -89,6 +89,8 @@ namespace GoodBookNook
                     pattern: "{controller=Home}/{action=Index}/{id?}");
             });
 
+            context.Database.Migrate();
+
             // Add a few books and reviews as sample data.
             SeedData.Seed(context);
 
