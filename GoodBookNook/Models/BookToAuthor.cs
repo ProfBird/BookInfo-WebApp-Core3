100cfb5579bd9422865bf4d34541752939c91219
Brian Bird <birdb@lanecc.edu>
2020-04-14 08:15:31 -0700

    New model

---
+++ GoodBookNook/Models/BookToAuthor.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GoodBookNook.Models
+{
+    public class Book
+    {
+        public int BookID { get; set; }
+        private List<Author> authors = new List<Author>();
+        private List<Review> reviews = new List<Review>();
+
+        [Required]
+        [StringLength(100, MinimumLength = 2)]
+        public string Title { get; set; }
+
+        [Required]
+        [DataType(DataType.Date)]  // causes default validation and sets the HTML5 data type
+        [DisplayFormat(DataFormatString = "{0:yyyy}")]  // only for display formatting with an HTML helper, doesn't validate
+        public DateTime PubDate { get; set; }
+
+        public List<Author> Authors { get { return authors; } }
+        public List<Review> Reviews { get { return reviews; } }
+    }
+}
